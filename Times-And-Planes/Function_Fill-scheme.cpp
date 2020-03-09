fb35056034d18dd9abdd04ecb4e024fd14ae8431
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-03-09 14:39:21 +0500

    Добавлена функция заполняющая схему

---
+++ Times-And-Planes/Function_Fill-scheme.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Антон on 09.03.2020.
+//
+
+#include <sstream>
+#include "Function_Fill-scheme.h"
+#include "Function_Find-IN.h"
+
+
+void fillScheme(string str, vector<int> &field)
+{
+    stringstream ss(str);
+    string tmp;
+    while (ss >> tmp)
+    {
+        field.push_back(findValueINpointNameToID(tmp));
+    }
+}
