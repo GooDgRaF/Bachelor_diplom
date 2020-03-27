c7dd92e8b2b202e6be089a6b8c2f88ae1de10287
GooDgRaF <tigr-mav@yandex.ru>
2020-03-27 14:46:03 +0500

    Много пояснений. Улучшено заполнение схемы. Добавлен отлов новых ошибок.

--- Times-And-Planes/Read_Scheme.cpp
+++ Times-And-Planes/Read_Scheme.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <regex>
 #include <fstream>
+#include <Functions/Function_Find-InVector.h>
 #include "Maps.h"
 #include "Scheme.h"
 #include "Functions/Function_OpenFile.h"
@@ -14,77 +15,95 @@
 
 using namespace std;
 
-void Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoint, vector<Scheme> &scheme)
-{
-    size_t k;
-
-    ifstream SchemeFile;
-
-    openFile(name_of_file, SchemeFile);
-
-    SchemeFile >> k;
-
-    scheme.resize(k);
-
-    /*
-     * Пояснение к регулярному выражению:
-     * Всего семь групп захвата
-     * NameA (POINT1)(POINT12 POINT13): POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
-     * 1) Название схемы [NameA]
-     * 2) Название точки начала схемы [POINT1]
-     * 3) Названия точек конца схемы [POINT12 POINT13]
-     * 4,6,7) Названия точек самой схемы [POINT2 + POINT3 POINT4 POINT5 + POINT12]
-     * 5) Названия точек куда можно спрямляться [POINT12 POINT13]
-     * 6) Названия точек откуда можно спрямляться [POINT3 POINT4 POINT5]
-     */
-
-    string str;
-    cmatch res;
-    regex regular(R"((\w+)\s*\((\w+)\)\s*\(([\w\s]+)\)\s*:\s*([\w\s]+)?\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)");
-    int i = 0;
-
-    string tmp;
-    getline(SchemeFile,tmp); //прочитать пустую строчку чтобы не мешалась
-
-    while (getline(SchemeFile,str))
+void Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes)
     {
-        regex_match(str.c_str(), res, regular);
+        size_t k;
 
+        ifstream SchemeFile;
 
+        openFile(name_of_file, SchemeFile);
 
-        try {
+        SchemeFile >> k; //Первым идёт количество схем
 
-        scheme[i].name = res[1];
+        schemes.resize(k);
 
-         pointIDtoStartSchemeID[findValueINpointNameToID(res[2])].push_back(i);
+        /*
+         * Пояснение к регулярному выражению:
+         * Всего семь групп захвата
+         * NameA (POINT1)(POINT12 POINT13): POINT1 POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
+         * 1) Название схемы [NameA]
+         * 2) Название точки начала схемы [POINT1]
+         * 3) Названия точек конца схемы [POINT12 POINT13]
+         * 4) Названия точек, предшествующих спрямлению [POINT2]
+         * 5) Названия точек куда можно спрямляться [POINT12 POINT13]
+         * 6) Названия точек откуда можно спрямляться [POINT3 POINT4 POINT5]
+         * 7) Названия точек следующих за спрямлением [POINT12]
+         * 4,6,7) Названия образующие path [POINT1 POINT2 + POINT3 POINT4 POINT5 + POINT12]
+         * Если спрямления нет, то 5,6,7 - пустые
+         */
 
-         scheme[i].start = findValueINpointNameToID(res[2]);
+        string str;
+        cmatch res;
+        regex regular(
+                R"((\w+)\s*\((\w+)\)\s*\(([\w\s]+)\)\s*:\s*([\w\s]+)?\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)");
+        int i = 0; // Счётчик, отвечающий за проход по schemes
 
-         fillScheme(res[3], scheme[i].end);
+        string tmp;
+        getline(SchemeFile, tmp); //прочитать пустую строчку чтобы не мешалась
 
-         fillScheme(res[5], scheme[i].straighteningWhere);
-
-         fillScheme(res[6], scheme[i].straighteningFrom);
-
-         fillScheme(res[4], scheme[i].middle);
-         fillScheme(res[6], scheme[i].middle);
-         fillScheme(res[7], scheme[i].middle);
-
-        }
-        catch(const runtime_error &ex)
+        while (getline(SchemeFile, str))
         {
-            cerr << "Can't find " << ex.what() << " in line " << i+2 << " among points" << endl;
-            exit(-2);
+            regex_match(str.c_str(), res, regular);
+
+            try //Заполняем соответственно комментарию о регулярном выражении
+            {
+                schemes[i].name = res[1];
+
+                pointIDtoStartSchemeID[findValueINpointNameToID(res[2])].push_back(i); //Заполняем отображение ID точки --> ID схемы, которая начинается в этой точке
+
+                schemes[i].start = findValueINpointNameToID(res[2]);
+
+                fillScheme(res[3], schemes[i].end);
+
+                fillScheme(res[5], schemes[i].straighteningWhere);
+
+                fillScheme(res[6], schemes[i].straighteningFrom);
+
+                fillScheme(res[4], schemes[i].path); //Собрали точки до спрямления
+
+                /*В ситуации, когда с начальной точки можно спрямляться возникает дублирование, необходимо его избежать
+                 * Name1 (POINT1)(POINT2 POINT3): POINT1 Str(POINT3) POINT1 POINT2 /Str
+                 * Поэтому, в такой ситуации path заполнять будем со второй точки с которых возможно спрямление
+                */
+                if (findInVector(schemes[i].straighteningFrom, schemes[i].start))
+                {
+                    for (int j = 1; j < schemes[i].straighteningFrom.size(); j++)
+                    {
+                        schemes[i].path.push_back(schemes[i].straighteningFrom[j]);
+                    }
+
+                }
+                else
+                {
+                    for (auto el : schemes[i].straighteningFrom)
+                    {
+                        schemes[i].path.push_back(el);
+                    }
+                }
+
+                fillScheme(res[7], schemes[i].path); //Собрали точки после спрямления
+
+            }
+            catch (const runtime_error &ex) //Ловим ошибку о не обнаружении точки из схемы среди точек из checkPoints
+            {
+                cerr << "Can't find " << ex.what() << " in line " << i + 2 << " among points" << endl;
+                exit(-2);
+            }
+
+            i++;
+
         }
 
-         i++;
+        SchemeFile.close();
 
     }
-
-    SchemeFile.close();
-
-
-
-
-
-}
