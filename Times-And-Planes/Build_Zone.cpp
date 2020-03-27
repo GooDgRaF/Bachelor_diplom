db477874121c00bffe36e759be37236a97718a57
GooDgRaF <tigr-mav@yandex.ru>
2020-03-27 14:40:09 +0500

    Добавил к описанию схем уточнение
    Теперь после двоеточия первой точкой указывается первая точка схемы

---
+++ Times-And-Planes/Build_Zone.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Антон on 23.03.2020.
+//
+
+#include <algorithm>
+#include <iostream>
+#include "Build_Zone.h"
+#include "Functions/Function_Find-InVector.h"
+
+void Build_Zone(const vector<Scheme> &schemes, vector<vector<int>> &graph_of_zone)
+    {
+        for (const auto &scheme : schemes) //Строим граф списками Следующий
+        {
+            /*Идём до size - 1, так как мы соединяем дугами со следующими точками,
+                 * то есть будет связь между предпоследней точкой и последней
+                 * и так как схемы соединяются в нахлёст, то есть конец одной схемы это начало другой, то все хорошо.
+                 */
+            for (int i = 0; i < scheme.path.size() - 1; i++)
+            {
+                graph_of_zone[scheme.path[i]].push_back(scheme.path[i + 1]); //Соединить текущую точку со следующей
+
+                /*Пояснение к следующему if:
+                *Если с текущей точки возможно спрямление и следующая точка не является конечной точкой спрямления
+                *Пояснение ко второй части if : Пример схема типа веер. Из её предпоследней точки можно спрямиться на центральную,
+                *а так же можно проследовать по пути, чтобы не повторяться существует проверка
+                */
+
+                if ((findInVector(scheme.straighteningFrom, scheme.path[i])
+                     & (!findInVector(scheme.straighteningWhere, scheme.path[i + 1]))))
+                {
+                    for (const auto &str : scheme.straighteningWhere) //Соединить текущую точку со всеми точками на которые возможно спрямление
+                    {
+                        graph_of_zone[scheme.path[i]].push_back(str);
+                    }
+                }
+            }
+
+        }
+
+        cout << "Build successes!" << endl;
+
+
+    }
---
+++ Times-And-Planes/Build_Zone.h
@@ -0,0 +1,17 @@
+//
+// Created by Антон on 23.03.2020.
+//
+
+#ifndef TIMES_AND_PLANES_BUILD_ZONE_H
+#define TIMES_AND_PLANES_BUILD_ZONE_H
+
+#include <vector>
+#include "CheckPoint.h"
+#include "Scheme.h"
+
+using namespace std;
+
+void Build_Zone(const vector<Scheme> &schemes, vector<vector<int>> &graph_of_zone);
+
+
+#endif //TIMES_AND_PLANES_BUILD_ZONE_H
