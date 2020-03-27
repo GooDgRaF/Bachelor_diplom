7a263b3b332527e073e9ee04ada797c45a893b8e
GooDgRaF <tigr-mav@yandex.ru>
2020-03-27 14:44:10 +0500

    Create Zone.h

---
+++ Times-And-Planes/Zone.h
@@ -0,0 +1,16 @@
+//
+// Created by Антон on 27.03.2020.
+//
+
+#ifndef TIMES_AND_PLANES_ZONE_H
+#define TIMES_AND_PLANES_ZONE_H
+
+#include <vector>
+
+using namespace std;
+
+struct Zone
+{
+	vector<vector<int>> graph_of_zone; //Задан списками Следующий
+};
+#endif //TIMES_AND_PLANES_ZONE_H
