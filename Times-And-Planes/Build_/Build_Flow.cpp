60331de80419b0eab210fedac4c9ec3539cdb7a2
GooDgRaF <tigr-mav@yandex.ru>
2020-03-28 15:07:12 +0500

    Moved to Build_

---
+++ Times-And-Planes/Build_/Build_Flow.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Антон on 28.03.2020.
+//
+
+#include "Build_Flow.h"
+
+void Build_Flow()
+{
+
+}
---
+++ Times-And-Planes/Build_/Build_Flow.h
@@ -0,0 +1,12 @@
+//
+// Created by Антон on 28.03.2020.
+//
+
+#ifndef TIMES_AND_PLANES_BUILD_FLOW_H
+#define TIMES_AND_PLANES_BUILD_FLOW_H
+
+using namespace std;
+
+#include <vector>
+
+#endif //TIMES_AND_PLANES_BUILD_FLOW_H
