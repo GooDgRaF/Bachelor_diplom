de1277cad3b4bad631d97df6de3beb5d0f8dcc91
GooDgRaF <tigr-mav@yandex.ru>
2020-03-28 15:10:29 +0500

    Update main.cpp

--- Times-And-Planes/main.cpp
+++ Times-And-Planes/main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 #include <vector>
-#include "Read_CheckPoints.h"
-#include "Read_Scheme.h"
+#include "Read_/Read_CheckPoints.h"
+#include "Read_/Read_Scheme.h"
 #include "CheckPoint.h"
 #include "Scheme.h"
 #include "Maps.h"
 #include "Flow.h"
-#include "Build_Flow.h"
+#include "Build_/Build_Zone.h"
+#include "Zone.h"
 #include "Functions/Topologic_Sort.h"
+#include "Read_/Read_Flow.h"
 
 using namespace std;
 
-int main() {
-
-    vector<CheckPoint> checkPoints;
-    vector<Scheme> schemes;
-    vector<Flow> flows (7);
-
-
-    string nameOfPointsFile = "CheckPointTestTopSort.txt";
-    Read_CheckPoints(nameOfPointsFile, checkPoints);
-
-    string nameOfSchemeFile = "SchemeTest.txt";
-    Read_Scheme(nameOfSchemeFile, checkPoints, schemes);
-
-    flows[0].graph_of_flow.resize(checkPoints.size());//????
-    Build_Flow(schemes, flows[0].graph_of_flow);
-
-    topologicalSort(flows[0].graph_of_flow, flows[0].keys);
-
-
-
-
-    return 0;
-}
+int main()
+	{
+		
+		vector<CheckPoint> checkPoints;
+		vector<Scheme> schemes;
+		vector<Flow> flows;
+		Zone zone;
+		
+		
+		string nameOfPointsFile = "CheckPointTestTwoFlows.txt";
+		Read_CheckPoints(nameOfPointsFile, checkPoints);
+		zone.graph.resize(checkPoints.size());
+		
+		
+		string nameOfSchemeFile = "SchemeTestTwoFlows.txt";
+		Read_Scheme(nameOfSchemeFile, checkPoints, schemes);
+		
+		
+		string nameOfFlowsFile = "TwoFlowsTest.txt";
+		Read_Flow(nameOfFlowsFile, flows);
+		
+		
+		Build_Zone(schemes, zone.graph);
+		
+		
+		//Методы для Zone: печать в разных вариантах
+		
+		zone.keys.resize(zone.graph.size());
+		topologicalSort(zone.graph, zone.keys);
+		
+		
+		return 0;
+	}
