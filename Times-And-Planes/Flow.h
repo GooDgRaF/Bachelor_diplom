1c3dec012e37073be8140f3b98eff184e0c8682c
GooDgRaF <tigr-mav@yandex.ru>
2020-03-28 17:11:18 +0500

    Update Flow.h

--- Times-And-Planes/Flow.h
+++ Times-And-Planes/Flow.h
@@ -13,7 +13,8 @@ struct Flow
 {
 	string name;
 	int start_point;
-    vector<vector<int>> graph; //Подграф графа Zone, так же заданный списками Следующий
+    vector<vector<int>> graph_of_descendants; //Подграф графа Zone, заданный списками Следующий
+    vector<vector<int>> graph_of_ancestors; //Подграф графа Zone, заданный списками Предшествующий
     vector<int> keys; //Массив для значений вершин после топологической сортировки
 };
 
