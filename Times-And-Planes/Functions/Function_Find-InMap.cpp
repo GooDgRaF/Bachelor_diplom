bde479cf4c8591f5496f95d86b30d7c06d00374b
GooDgRaF <tigr-mav@yandex.ru>
2020-03-29 20:08:33 +0500

    Update Function_Find-InMap.cpp

--- Times-And-Planes/Functions/Function_Find-InMap.cpp
+++ Times-And-Planes/Functions/Function_Find-InMap.cpp
@@ -7,7 +7,7 @@
 
 int findValueINpointNameToID(const string &Key)
 	{
-		map<string, int>::iterator res = pointNameToID.find(Key);
+		auto res = pointNameToID.find(Key);
 		if (res == pointNameToID.end()) //Неудачный поиск, сообщить об ошибке
 		{
 			throw runtime_error(Key);
