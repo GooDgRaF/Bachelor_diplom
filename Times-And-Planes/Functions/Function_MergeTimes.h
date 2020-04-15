19b2042fd08bf674129110593259b5d4b3a38175
GooDgRaF <tigr-mav@yandex.ru>
2020-04-15 10:13:04 +0500

    Create Function_MergeTimes.h

---
+++ Times-And-Planes/Functions/Function_MergeTimes.h
@@ -0,0 +1,14 @@
+//
+// Created by Антон on 14.04.2020.
+//
+
+#ifndef TIMES_AND_PLANES_FUNCTION_MERGETIMES_H
+#define TIMES_AND_PLANES_FUNCTION_MERGETIMES_H
+
+#include <vector>
+
+using namespace std;
+
+void mergeTimes(vector<pair<int, int>> &times);
+
+#endif //TIMES_AND_PLANES_FUNCTION_MERGETIMES_H
