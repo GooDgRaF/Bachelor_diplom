0d823ae430d54fdaaa18de6f5e7b65aed110841d
GooDgRaF <tigr-mav@yandex.ru>
2020-04-16 21:55:17 +0500

    Update Flow.h

--- Times-And-Planes/Flow.h
+++ Times-And-Planes/Flow.h
@@ -8,14 +8,15 @@
 #include <vector>
 #include <string>
 #include <map>
+
 using namespace std;
 
 struct Flow
 {
 	string name;
 	int start_point;
-	map<int,vector<int>> graph_of_descendants; //Подграф графа Zone, заданный списками Следующий
-	map<int,vector<int>> graph_of_ancestors; //Подграф графа Zone, заданный списками Предшествующий
+	map<int, vector<int>> graph_of_descendants; //Подграф графа Zone, заданный списками Следующий
+	map<int, vector<int>> graph_of_ancestors; //Подграф графа Zone, заданный списками Предшествующий
 	vector<int> keys; //Массив для значений вершин после топологической сортировки
 };
 
