a5a688a02afc4b7cedd57567c893161089e4d30d
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-05-13 18:50:21 +0500

    Переименовал

--- Times-And-Planes/Read/Read_SchemeRegExp.cpp
+++ Times-And-Planes/Read/Read_SchemeRegExp.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 void
-Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<Cycle> &cycles)
+Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<Cycle> &cycles)
 	{
 		int i, k;
 		
@@ -27,30 +27,38 @@ Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<
 		SchemeFile >> i; //Первым идёт количество схем
 		SchemeFile >> k; //Потом количество циклов
 		
+		if ((k < 0) || (i <= 0))
+		{
+			cerr << "Warning put the correct value for the number of schemes or cycles in " << name_of_file << endl;
+			exit(-2);
+		}
+		
+		
 		schemes.resize(i);
 		cycles.resize(k);
 		
 		/*
 		 * Пояснение к регулярному выражению:
 		 * Всего восемь групп захвата:
-		 * NameA (POINT1)(POINT12 POINT13)(LAND | 7): POINT1 POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
+		 * NameA (POINT1)(POINT12 POINT13)(number): POINT1 POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
 		 * 1) Название схемы [NameA]
 		 * 2) Название точки начала схемы [POINT1]
 		 * 3) Названия точек конца схемы [POINT12 POINT13]
-		 * 4) Вспомогательная группа, посадочная метка (приписывается всем точкам этой схемы) либо количество повторений цикла
+		 * 4) Вспомогательная группа, количество повторений цикла
 		 * 5) Названия точек, предшествующих спрямлению [POINT1 POINT2]
 		 * 6) Названия точек куда можно спрямляться [POINT12 POINT13]
 		 * 7) Названия точек откуда можно спрямляться [POINT3 POINT4 POINT5]
 		 * 8) Названия точек следующих за спрямлением [POINT12]
 		 * 5,7,8) Названия образующие path [POINT1 POINT2 + POINT3 POINT4 POINT5 + POINT12]
-		 * Если цикла нет, посадки тоже - 4 группа пустая
+		 * Если цикл есть то 3,6,7,8 группа пустые
+		 * Если цикла нет - 4 группа пустая
 		 * Если спрямления нет, то 6,7,8 - пустые
 		 */
 		
 		string str;
 		cmatch res;
 		regex regular(
-				R"((\w+)\s*\((\w+)\)\s*\(([\w\s]+)\)\s*(?:\((LAND|\d+)\)\s*)?:\s*([\w\s]+)?\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)");
+				R"((\w+)\s*\(([a-z,A-Z]\w*)\)\s*(?:\(([a-z,A-Z][\w\s]*)\))?\s*(?:\((\d+)\)\s*)?:\s*([\w\s]+)?\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)");
 		i = 0; // Счётчик, отвечающий за проход по schemes
 		k = 0; // Счётчик, отвечающий за проход по cycles
 		
@@ -70,6 +78,17 @@ Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<
 			{
 				try //Заполняем соответственно комментарию о регулярном выражении
 				{
+					for (const auto &scheme : schemes) //Проверка, чтобы две схемы с одинаковым названием не встречались
+					{
+						if (scheme.name == res[1])
+						{
+							cerr << "Attention! '" << res[1] << "' scheme occurs twice in " << name_of_file << endl;
+							exit(-2);
+						}
+					}
+					
+					schemes.at(i);
+					
 					schemes[i].name = string(res[1]);
 					
 					schemes[i].start = pointNameToID.at(string(res[2]));
@@ -96,6 +115,11 @@ Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<
 						 << endl;
 					exit(-2);
 				}
+				catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
+				{
+					cerr << "Check in " << name_of_file << " amount of schemes, it is low" << endl;
+					exit(-2);
+				}
 				
 				if (res[4] == "LAND")
 				{
@@ -108,23 +132,29 @@ Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<
 			}
 			else
 			{
-				cycles[k].name = string(res[1]);
-				
-				cycles[k].start = pointNameToID[res[2]];
-				
-				cycles[k].finish = pointNameToID[res[3]];
-				
-				cycles[k].repeat = atoi(res[4].first);
-				
 				try
 				{
+					cycles.at(k);
+					
+					cycles[k].name = string(res[1]);
+					
+					cycles[k].start = pointNameToID[res[2]];
+					
+					cycles[k].repeat = atoi(res[4].first);
+					
 					fillScheme(string(res[5]), cycles[k].path);
 					fillScheme(string(res[8]), cycles[k].path); //Вспомогательная добавка, на случай, если нет пробела после последней точки
 				}
 				catch (const runtime_error &ex) //Ловим ошибку о не обнаружении точки из цикла среди точек из checkPoints
 				{
-					cerr << "Can't find '" << ex.what() << "' in line '" << i + k + 3 << "' among points from Points file"
+					cerr << "Can't find '" << ex.what() << "' in line '" << i + k + 3
+						 << "' among points from Points file"
 						 << endl;
+					exit(-2);
+				}
+				catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
+				{
+					cerr << "Check in " << name_of_file << " amount of cycles, it is low" << endl;
 					exit(-2);
 				}
 				
@@ -133,9 +163,9 @@ Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<
 				k++;
 				
 			}
-			
-			
 		}
+		schemes.resize(i); //На случай если число схем, считанное из файла больше действительного их количества
+		cycles.resize(k); //На случай если число циклов, считанное из файла больше действительного их количества
 		
 		SchemeFile.close();
 		
--- Times-And-Planes/Read/Read_SchemeRegExp.h
+++ Times-And-Planes/Read/Read_SchemeRegExp.h
@@ -2,8 +2,8 @@
 // Created by Антон on 10.02.2020.
 //
 
-#ifndef TIMES_AND_PLANES_READ_SCHEME_H
-#define TIMES_AND_PLANES_READ_SCHEME_H
+#ifndef TIMES_AND_PLANES_READ_SCHEMEREGEXP_H
+#define TIMES_AND_PLANES_READ_SCHEMEREGEXP_H
 
 #include <string>
 #include <vector>
@@ -15,6 +15,6 @@ using namespace std;
 
 
 void
-Read_Scheme(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<Cycle> &cycles);
+Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<Cycle> &cycles);
 
 #endif
