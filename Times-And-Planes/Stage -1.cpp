cd04d467a3ccd3bd11e39bb0d78690acdec11dd8
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-05-13 18:56:46 +0500

    Update Stage -1.cpp

--- Times-And-Planes/Stage -1.cpp
+++ Times-And-Planes/Stage -1.cpp
@@ -5,9 +5,8 @@
 #include <Build/Build_Flow.h>
 #include <Read/Read_CheckPointsRegExp.h>
 #include "Stage -1.h"
-#include "Read/Read_CheckPoints.h"
-#include "Read/Read_Scheme.h"
-#include "Read/Read_Flow.h"
+#include "Read/Read_SchemeRegExp.h"
+#include "Read/Read_FlowRegExp.h"
 #include "Functions/Topologic_Sort.h"
 
 void stage_minus_one(string &path_checkPointsFile, string &path_SchemesFile, string &path_FlowsFile, Zone &zone)
@@ -15,9 +14,9 @@ void stage_minus_one(string &path_checkPointsFile, string &path_SchemesFile, str
 		Read_CheckPointsRegExp(path_checkPointsFile, zone.checkPoints);
 		zone.graph_of_descendants.resize(zone.checkPoints.size());
 		
-		Read_Scheme(path_SchemesFile, zone.checkPoints, zone.schemes, zone.cycles);
+		Read_SchemeRegExp(path_SchemesFile, zone.checkPoints, zone.schemes, zone.cycles);
 		
-		Read_Flow(path_FlowsFile, zone.flows);
+		Read_FlowRegExp(path_FlowsFile, zone.flows);
 		
 		Build_Zone(zone);
 		
