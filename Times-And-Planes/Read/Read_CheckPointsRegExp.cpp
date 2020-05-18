345e5d098e5f811d3fcaffce2d96f5cfe5ce4bdf
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-05-18 11:53:38 +0500

    Имя точки теперь начинается с буквы

--- Times-And-Planes/Read/Read_CheckPointsRegExp.cpp
+++ Times-And-Planes/Read/Read_CheckPointsRegExp.cpp
@@ -32,7 +32,7 @@ void Read_CheckPointsRegExp(const std::string &name_of_file, std::vector<CheckPo
 		 * Пояснение к регулярному выражению:
 		 * Всего 7 групп захвата, согласно формату:
 		 * NameP x y z v1 v2 land_flag
-		 * 1) - имя точки [NameP]
+		 * 1) - имя точки [NameP], начинается с буквы
 		 * 2,3,4) - координаты x,y,z
 		 * 5,6) - скорости vmin, vmax
 		 * 7) - флаг посадочной полосы - LAND или пусто либо 0
@@ -40,7 +40,7 @@ void Read_CheckPointsRegExp(const std::string &name_of_file, std::vector<CheckPo
 		
 		string str;
 		cmatch res;
-		regex regular(R"((\w+)\s+([-+]?[0-9]*\.?[0-9]+)\s+([-+]?[0-9]*\.?[0-9]+)\s+([-+]?[0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)(?:\s*)(LAND|0)?)");
+		regex regular(R"(([a-z,A-Z]\w*)\s+([-+]?[0-9]*\.?[0-9]+)\s+([-+]?[0-9]*\.?[0-9]+)\s+([-+]?[0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)(?:\s*)(LAND|0)?)");
 		
 		i = 0; // Счётчик, отвечающий за проход по chekpoints
 		
@@ -67,23 +67,27 @@ void Read_CheckPointsRegExp(const std::string &name_of_file, std::vector<CheckPo
 			
 			
 			double x, y, z, vmin, vmax;
-			
-			x = atof(res[2].first);
-			y = atof(res[3].first);
-			z = atof(res[4].first);
-			vmin = atof(res[5].first);
-			vmax = atof(res[6].first);
-			
 			try
 			{
-				checkPoints.at(i);
+				checkPoints.at(i); //Проверка доступа к i-ой координате вектора checkPoints
+				
+				x = stod(string(res[2]));
+				y = stod(string(res[3]));
+				z = stod(string(res[4]));
+				vmin = stod(string(res[5]));
+				vmax = stod(string(res[6]));
 			}
 			catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
 			{
 				cerr << "Check in " << name_of_file << " amount of points, it is low" << endl;
 				exit(-2);
 			}
-			
+			catch (const invalid_argument &ex) //Не бывает
+			{
+				cerr << "Warning! Line '" << i + 2 << "' in " << name_of_file << " doesn't follow the input format"
+					 << endl;
+				exit(-3);
+			}
 			
 			checkPoints[i].name = res[1];
 			
@@ -91,7 +95,7 @@ void Read_CheckPointsRegExp(const std::string &name_of_file, std::vector<CheckPo
 			checkPoints[i].y = Coordinate::createMs(y); //В зависимости от исходных данных
 			checkPoints[i].z = Coordinate::createMs(z);
 			
-			checkPoints[i].Vmin = Velocity::createVm_s(vmin);
+			checkPoints[i].Vmin = Velocity::createVm_s(vmin); //В зависимости от исходных данных
 			checkPoints[i].Vmax = Velocity::createVm_s(vmax);
 			
 			checkPoints[i].landing_flag = res[7] == "LAND";
