2f277eca7ee3cecda3dbc6496325aea415604623
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-05-18 11:54:34 +0500

    Переход от циклов к стандартной схеме + немного упрощений для вводда схем (см Формат)

--- Times-And-Planes/Read/Read_SchemeRegExp.cpp
+++ Times-And-Planes/Read/Read_SchemeRegExp.cpp
@@ -7,7 +7,7 @@
 #include <regex>
 #include <fstream>
 #include <Functions/Find-InVector.h>
-#include <Fields of Zone/Cycle.h>
+#include <Fields of Zone/StandardScheme.h>
 #include "Fields of Zone/Maps.h"
 #include "Fields of Zone/Scheme.h"
 #include "Functions/OpenFile.h"
@@ -15,8 +15,21 @@
 
 using namespace std;
 
+bool stoi_bool(const string &str)
+	{
+		try
+		{
+			stoi(str);
+			return true;
+		}
+		catch (invalid_argument &ex)
+		{
+			return false;
+		}
+	} //Истина, если строка является числом, Ложь иначе
+
 void
-Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<Cycle> &cycles)
+Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, vector<Scheme> &schemes, vector<StandardScheme> &standardSchemes)
 	{
 		int i, k;
 		
@@ -25,56 +38,52 @@ Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, v
 		openFile(name_of_file, SchemeFile);
 		
 		SchemeFile >> i; //Первым идёт количество схем
-		SchemeFile >> k; //Потом количество циклов
+		SchemeFile >> k; //Потом количество стандартных схем
 		
-		if ((k < 0) || (i <= 0))
+		if ((i <= 0) || (k < 0))
 		{
-			cerr << "Warning put the correct value for the number of schemes or cycles in " << name_of_file << endl;
+			cerr << "Warning put the correct value for the number of schemes or standard schemes in " << name_of_file
+				 << endl;
 			exit(-2);
 		}
 		
 		
 		schemes.resize(i);
-		cycles.resize(k);
+		standardSchemes.resize(k);
 		
 		/*
 		 * Пояснение к регулярному выражению:
-		 * Всего восемь групп захвата:
-		 * NameA (POINT1)(POINT12 POINT13)(number): POINT1 POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
+		 * Всего семь групп захвата:
+		 * NameA (POINT1)(POINT12 POINT13 | number): POINT1 POINT2 Str(POINT12 POINT13) POINT3 POINT4 POINT5 /Str POINT12
 		 * 1) Название схемы [NameA]
 		 * 2) Название точки начала схемы [POINT1]
-		 * 3) Названия точек конца схемы [POINT12 POINT13]
-		 * 4) Вспомогательная группа, количество повторений цикла
-		 * 5) Названия точек, предшествующих спрямлению [POINT1 POINT2]
-		 * 6) Названия точек куда можно спрямляться [POINT12 POINT13]
-		 * 7) Названия точек откуда можно спрямляться [POINT3 POINT4 POINT5]
-		 * 8) Названия точек следующих за спрямлением [POINT12]
-		 * 5,7,8) Названия образующие path [POINT1 POINT2 + POINT3 POINT4 POINT5 + POINT12]
-		 * Если цикл есть то 3,6,7,8 группа пустые
-		 * Если цикла нет - 4 группа пустая
-		 * Если спрямления нет, то 6,7,8 - пустые
+		 * 3) Названия точек конца схемы [POINT12 POINT13] или число повторений стандарной схемы
+		 * 4) Названия точек, предшествующих спрямлению [POINT1 POINT2]
+		 * 5) Названия точек куда можно спрямляться [POINT12 POINT13]
+		 * 6) Названия точек откуда можно спрямляться [POINT3 POINT4 POINT5]
+		 * 7) Названия точек следующих за спрямлением [POINT12]
+		 * 4,6,7) Названия образующие path [POINT1 POINT2 + POINT3 POINT4 POINT5 + POINT12]
 		 */
 		
 		string str;
 		cmatch res;
 		regex regular(
-				R"((\w+)\s*\(([a-z,A-Z]\w*)\)\s*(?:\(([a-z,A-Z][\w\s]*)\))?\s*(?:\((\d+)\)\s*)?:\s*([\w\s]+)?\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)");
+				R"((\w+)\s*(?:\((\w+)\))?\s*\(([\w\s]*|\d+)\)\s*:(?:\s*([\w\s]*)\s+(?:Str\(([\w\s]+)\)\s*([\w\s]+)\/Str)?\s*([\w\s]*)?)?)");
 		i = 0; // Счётчик, отвечающий за проход по schemes
-		k = 0; // Счётчик, отвечающий за проход по cycles
+		k = 0; // Счётчик, отвечающий за проход по standardSchemes
 		
-		string tmp;
-		getline(SchemeFile, tmp); //прочитать пустую строчку чтобы не мешалась
+		getline(SchemeFile, str); //прочитать пустую строчку чтобы не мешалась
 		
 		while (getline(SchemeFile, str))
 		{
 			if (!regex_match(str.c_str(), res, regular))
 			{
-				cerr << "Warning! Line '" << i + 3 << "' in " << name_of_file << " doesn't follow the input format"
+				cerr << "Warning! Line '" << i + k + 3 << "' in " << name_of_file << " doesn't follow the input format"
 					 << endl;
 				exit(-3);
 			}
 			
-			if ((string(res[4]).empty()) || res[4] == "LAND") //Текущая схема не цикл
+			if (!stoi_bool(string(res[3]))) //Если не стандартная схема
 			{
 				try //Заполняем соответственно комментарию о регулярном выражении
 				{
@@ -87,65 +96,43 @@ Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, v
 						}
 					}
 					
-					schemes.at(i);
+					schemes.at(i); //Проверка доступа к i-ой координате вектора schemes
 					
-					schemes[i].name = string(res[1]);
-					
-					schemes[i].start = pointNameToID.at(string(res[2]));
-					
-					fillScheme(string(res[3]), schemes[i].end);
-					
-					fillScheme(string(res[6]), schemes[i].straighteningWhere);
-					
-					fillScheme(string(res[7]), schemes[i].straighteningFrom);
-					
-					fillScheme(string(res[5]), schemes[i].path); //Собрали точки до спрямления
-					
-					for (auto el : schemes[i].straighteningFrom) //Собрали точки с которых возможно спрямление
+					if (string(res[4]).empty() && string(res[5]).empty()) //Схема из двух точек
 					{
-						schemes[i].path.push_back(el);
+						schemes[i].name = string(res[1]);
+						
+						schemes[i].start = pointNameToID.at(string(res[2]));
+						
+						schemes[i].end.push_back(pointNameToID.at(string(res[3])));
+						
+						schemes[i].path.push_back(schemes[i].start);
+						schemes[i].path.push_back(schemes[i].end[0]);
 					}
-					
-					fillScheme(string(res[8]), schemes[i].path); //Собрали точки после спрямления
-					
+					else //Все остальные схемы
+					{
+						schemes[i].name = string(res[1]);
+						
+						schemes[i].start = pointNameToID.at(string(res[2]));
+						
+						fillScheme(string(res[3]), schemes[i].end);
+						
+						fillScheme(string(res[5]), schemes[i].straighteningWhere);
+						
+						fillScheme(string(res[6]), schemes[i].straighteningFrom);
+						
+						fillScheme(string(res[4]), schemes[i].path); //Собрали точки до спрямления
+						
+						for (auto el : schemes[i].straighteningFrom) //Собрали точки с которых возможно спрямление
+						{
+							schemes[i].path.push_back(el);
+						}
+						
+						fillScheme(string(res[7]), schemes[i].path); //Собрали точки после спрямления
+						
+					}
 				}
 				catch (const runtime_error &ex) //Ловим ошибку о не обнаружении точки из схемы среди точек из checkPoints
-				{
-					cerr << "Can't find '" << ex.what() << "' in line '" << i + 3 << "' among points from Points file"
-						 << endl;
-					exit(-2);
-				}
-				catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
-				{
-					cerr << "Check in " << name_of_file << " amount of schemes, it is low" << endl;
-					exit(-2);
-				}
-				
-				if (res[4] == "LAND")
-				{
-					for (auto el : schemes[i].path)
-					{
-						checkPoints[el].landing_flag = true;
-					}
-				}
-				i++;
-			}
-			else
-			{
-				try
-				{
-					cycles.at(k);
-					
-					cycles[k].name = string(res[1]);
-					
-					cycles[k].start = pointNameToID[res[2]];
-					
-					cycles[k].repeat = atoi(res[4].first);
-					
-					fillScheme(string(res[5]), cycles[k].path);
-					fillScheme(string(res[8]), cycles[k].path); //Вспомогательная добавка, на случай, если нет пробела после последней точки
-				}
-				catch (const runtime_error &ex) //Ловим ошибку о не обнаружении точки из цикла среди точек из checkPoints
 				{
 					cerr << "Can't find '" << ex.what() << "' in line '" << i + k + 3
 						 << "' among points from Points file"
@@ -154,18 +141,67 @@ Read_SchemeRegExp(const string &name_of_file, vector<CheckPoint> &checkPoints, v
 				}
 				catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
 				{
-					cerr << "Check in " << name_of_file << " amount of cycles, it is low" << endl;
+					cerr << "Check in " << name_of_file << " amount of schemes, it is low" << endl;
+					exit(-2);
+				}
+				i++;
+			}
+			else
+			{
+				try
+				{
+					standardSchemes.at(k); //Проверка доступа к i-ой координате вектора standardSchemes
+					
+					standardSchemes[k].name = string(res[1]);
+					
+					standardSchemes[k].repeat = stoi(string(res[3]));
+					
+					vector<int> path; //Вспомогательный вектор для заполнения полей стандартной схемы
+					
+					fillScheme(string(res[4]), path);
+					fillScheme(string(res[7]), path); //Если нет пробела после последней точки, то нужно добавить эту точку
+					
+					if (path.size() != 3) //Стандартная схема описывается тремя точками
+					{
+						cerr << "Warning! Line '" << i + k + 3 << "' in " << name_of_file
+							 << " doesn't follow the input format: the standard scheme should have three points"
+							 << endl;
+						exit(-3);
+					}
+					
+					standardSchemes[k].start = path[0];
+					standardSchemes[k].second = path[1];
+					standardSchemes[k].third = path[2];
+					
+				}
+				catch (const runtime_error &ex) //Ловим ошибку о не обнаружении точки из стандартной схемы среди точек из checkPoints
+				{
+					cerr << "Can't find '" << ex.what() << "' in line '" << i + k + 3
+						 << "' among points from Points file"
+						 << endl;
+					exit(-2);
+				}
+				catch (const out_of_range &ex) //Ловим ошибку о нехватке выделенного места для входных данных
+				{
+					cerr << "Check in " << name_of_file << " amount of standardSchemes, it is low" << endl;
 					exit(-2);
 				}
 				
-				pointIDtoStartCycleID[cycles[k].start] = k;
+				pointIDtoStartCycleID[standardSchemes[k].start] = k;
 				
 				k++;
 				
 			}
 		}
-		schemes.resize(i); //На случай если число схем, считанное из файла больше действительного их количества
-		cycles.resize(k); //На случай если число циклов, считанное из файла больше действительного их количества
+		if (schemes.size() > i) //На случай если число схем, считанное из файла больше действительного их количества
+		{
+			schemes.resize(i);
+		}
+		if (standardSchemes.size() >
+			k) //На случай если число стандартных схем, считанное из файла больше действительного их количества
+		{
+			standardSchemes.resize(k);
+		}
 		
 		SchemeFile.close();
 		
