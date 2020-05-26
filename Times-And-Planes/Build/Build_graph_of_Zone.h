9a9bdfdcd4cb21cbef746c5a277994bf6ba516f7
Anton Mikhailov <53288934+GooDgRaF@users.noreply.github.com>
2020-05-26 20:14:09 +0500

    Переименовал

--- Times-And-Planes/Build/Build_graph_of_Zone.h
+++ Times-And-Planes/Build/Build_graph_of_Zone.h
@@ -2,8 +2,8 @@
 // Created by Антон on 23.03.2020.
 //
 
-#ifndef TIMES_AND_PLANES_BUILD_ZONE_H
-#define TIMES_AND_PLANES_BUILD_ZONE_H
+#ifndef TIMES_AND_PLANES_BUILD_GRAPH_OF_ZONE_H
+#define TIMES_AND_PLANES_BUILD_GRAPH_OF_ZONE_H
 
 #include <vector>
 #include <Zone.h>
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-void Build_Zone(Zone &zone);
+void Build_graph_of_Zone(Zone &zone);
 
 
-#endif //TIMES_AND_PLANES_BUILD_ZONE_H
+#endif //TIMES_AND_PLANES_BUILD_GRAPH_OF_ZONE_H
